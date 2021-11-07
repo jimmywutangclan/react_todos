43d99d6308da9ac9609b2862d38aa224c3319665
gamerdudesnip3r <jw29731@gmail.com>
2021-11-06 21:22:05 -0400

    Added local storage

--- src/Components/TasksApp.js
+++ src/Components/TasksApp.js
@@ -5,31 +5,35 @@ import TaskList from './TaskList';
 export default class TasksApp extends Component {
     constructor(props) {
         super(props);
-        this.state = { tasks: [] };
+        this.state = { tasks: localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [] };
     }
 
     createTask = (task) => {
         const taskList = this.state.tasks;
         taskList.push({ task: task, completed: false });
         this.setState({ tasks: taskList });
+        localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
     }
 
     deleteTask = (index) => {
         const taskList = this.state.tasks;
         taskList.splice(index, 1);
         this.setState({ tasks: taskList });
+        localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
     }
     
     editTask = (task, index) => {
         const taskList = this.state.tasks;
         taskList[index].task = task;
         this.setState({ tasks: taskList });
+        localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
     }
 
     changeStatus = (index) => {
         const taskList = this.state.tasks;
         taskList[index].completed = !taskList[index].completed;
         this.setState({tasks: taskList});
+        localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
     }
 
 
